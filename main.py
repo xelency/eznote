c4fb388cd3349e33650ec32c604fdd98c4f21e38
Xelency <game.server@windowslive.com>
2020-04-17 12:53:11 +0300

    add entry and layout tempates

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def homePage():
     return '<H1>Hello World</H1>'
+
+@app.route('/entry')
+def entryPage():
+    return render_template('entry.html', title='Compose New Entry')
+
+## This is to allow us to run on debug mode directly with python command line.
+if __name__ == '__main__':
+    app.run(debug=True)
