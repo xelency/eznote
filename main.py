16c0a16e1a722b3533f7b2fe3b903259593afbf3
Xelency <game.server@windowslive.com>
2020-04-18 12:35:27 +0300

    Add form to entry page and fix function names on main to pythonic

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
-
+from forms import EntryForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'somekey'
+
 @app.route('/')
-def homePage():
-    return '<H1>Hello World</H1>'
+def Page_Home():
+	return render_template('home.html', title='Welcome')
 
 @app.route('/entry')
-def entryPage():
-    return render_template('entry.html', title='Compose New Entry')
+def Page_Entry():
+    return render_template('entry.html', title='Compose New Entry', form=EntryForm())
 
 ## This is to allow us to run on debug mode directly with python command line.
 if __name__ == '__main__':
